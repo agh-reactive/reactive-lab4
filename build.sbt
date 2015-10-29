fda2251ace919629e8dc2d1371309d3bc9401279
Maciej Malawski <malawski@agh.edu.pl>
2015-10-29 22:14:15 +0100

    Added persistent FSM example

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "reactive-lab4"
+
+scalaVersion := "2.11.7" 
+
+fork := true
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.4.0",
+  "com.typesafe.akka" %% "akka-persistence" % "2.4.0",
+  "org.iq80.leveldb" % "leveldb" % "0.7",
+  "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8"
+)
+
---
+++ src/main/scala/reactive4/persistence/PersistentToggle.scala
@@ -0,0 +1,90 @@
+package reactive4.persistence
+
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.event.LoggingReceive
+import akka.persistence._
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import akka.actor.actorRef2Scala
+
+
+// states
+sealed trait MoodState
+case object Happy extends MoodState
+case object Sad extends MoodState
+
+case class MoodChangeEvent(state: MoodState)
+
+class PersistentToggle extends PersistentActor {
+
+  override def persistenceId = "persistent-toggle-id-1"
+
+  def updateState(event: MoodChangeEvent): Unit =
+    context.become(
+      event.state match {
+        case Happy => happy
+        case Sad => sad
+      })
+
+  def happy: Receive = LoggingReceive {
+    case "How are you?" =>
+      persist(MoodChangeEvent(Sad)) {
+        event =>
+          updateState(event)
+          sender ! "happy"
+      }
+    case "Done" =>
+      sender ! "Done"
+      context.stop(self)
+  }
+
+  def sad: Receive = LoggingReceive {
+    case "How are you?" =>
+      persist(MoodChangeEvent(Happy)) {
+        event =>
+          updateState(event)
+          sender ! "sad"
+      }
+
+    case "Done" =>
+      sender ! "Done"
+
+  }
+  def receiveCommand = happy
+
+  val receiveRecover: Receive = {
+    case evt: MoodChangeEvent => updateState(evt)
+  }
+}
+
+class ToggleMain extends Actor {
+
+  val toggle = context.actorOf(Props[PersistentToggle], "toggle")
+
+  toggle ! "How are you?"
+  toggle ! "How are you?"
+  toggle ! "How are you?"
+  toggle ! "Done"
+
+  def receive = LoggingReceive {
+
+    case "Done" =>
+      context.system.terminate()
+
+    case msg: String =>
+      println(s" received: $msg")
+
+  }
+}
+
+
+object ToggleApp extends App {
+  val system = ActorSystem("Reactive4")
+  val mainActor = system.actorOf(Props[ToggleMain], "mainActor")
+
+  mainActor ! "Init"
+
+  Await.result(system.whenTerminated, Duration.Inf)
+}
